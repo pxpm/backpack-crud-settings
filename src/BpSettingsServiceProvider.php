72d7dc4e11b5fae68b30c4141225e05923f24463
Pedro X <pxpm88@gmail.com>
2020-01-22 13:56:09 +0000

    initial commit

---
+++ src/BpSettingsServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Pxpm\BpSettings;
+
+use Illuminate\Support\ServiceProvider;
+
+class BpSettingsServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+
+    public function boot()
+    {
+
+
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+}
